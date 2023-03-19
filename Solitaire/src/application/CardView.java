82b254925f19e49ef2ab737f464d968e4be5d3cc
Tirigan <matthew.ray.ayres@gmail.com>
2023-03-19 17:29:14 -0400

    attempting to get some cards moving, still not working

--- Solitaire/src/application/CardView.java
+++ Solitaire/src/application/CardView.java
@@ -1,28 +1,58 @@
 package application;
 
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import models.Card;
+import javafx.scene.input.ClipboardContent;
 
-public class CardView {
-	private final Card card;
-	private final Image faceDownImage = new Image(CardView.class.getResource("/images/card-face-down.jpeg").toExternalForm());
-	private final Image faceUpImage;
+public class CardView extends ImageView {
 
-	CardView(Card card) {
-		this.card = card;
-		this.faceUpImage = new Image(CardView.class.getResource("/images/card-"+card.getSuit().name()+"-"+card.getSuit().name()+".png").toExternalForm());
-	}
+    private final Card card;
+    private final Image faceDownImage = new Image(CardView.class.getResource("/images/card-face-down.jpeg").toExternalForm());
+    private final Image faceUpImage;
 
-	public Card getCard() {
-		return card;
-	}
+    private double startX, startY;
 
-	public ImageView getCardImageView() {
-		if(card.isFaceUp()) {
-			return new ImageView(faceUpImage);
-		}
-		return new ImageView(faceDownImage);
-	}
-	
+    public CardView(Card card) {
+        super();
+        this.card = card;
+        this.faceUpImage = new Image(CardView.class.getResource("/images/card-" + card.getSuit().name() + "-" + card.getRank().name() + ".png").toExternalForm());
+        setImage(faceDownImage);
+        setOnMousePressed(event -> {
+            startX = event.getSceneX();
+            startY = event.getSceneY();
+        });
+        setOnMouseDragged(event -> {
+            double offsetX = event.getSceneX() - startX;
+            double offsetY = event.getSceneY() - startY;
+            setTranslateX(offsetX);
+            setTranslateY(offsetY);
+        });
+        setOnMouseReleased(event -> {
+            // Add code to snap card to appropriate position on release, if desired
+        });
+        setOnDragDetected(event -> {
+            Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString("card");
+            dragboard.setContent(content);
+            event.consume();
+        });
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void showFaceUp() {
+        setImage(faceUpImage);
+    }
+
+    public void showFaceDown() {
+        setImage(faceDownImage);
+    }
 }
