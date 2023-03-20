26fbc1866d4b091f5ba53d38faaec673068e3241
hatch47 <codyhatcher11@yahoo.com>
2023-03-20 17:41:56 -0400

    Score, Timer, GameOver class added

--- Solitaire/src/application/GameOver.java
+++ Solitaire/src/application/GameOver.java
@@ -39,7 +39,7 @@ public class GameOver {
 	    owner.setTitle("Game Over");
 
 	    // Create a label for the title
-	    Label titleLabel = new Label("Winner! Congratulations!");
+	    Label titleLabel = new Label("You Win! Congratulations!");
 	    titleLabel.setFont(Font.font("Arial", FontWeight.BOLD, 36));
 	    titleLabel.setAlignment(Pos.CENTER);
 	    titleLabel.setLayoutX(0);
@@ -75,66 +75,6 @@ public class GameOver {
 	    // Show the window
 	    owner.show();
 	}
-
-
-//	public static void showGameOver(Stage owner, TimerTask timerTask) {
-//	    // Create a pane to hold our content
-//	    Pane root = new Pane();
-//
-//	    // Set the background color to green
-//	    root.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
-//
-//	    // Set the window size to 900x600 and make it not resizable
-//	    Scene scene = new Scene(root, 900, 600);
-//	    owner.setScene(scene);
-//	    owner.setResizable(false);
-//
-//	    // Set the title of the window
-//	    owner.setTitle("Game Over");
-//
-//	    // Create a label for the title
-//	    Label titleLabel = new Label("Winner! Congratulations!");
-//	    titleLabel.setFont(Font.font("Arial", FontWeight.BOLD, 36));
-//	    titleLabel.setAlignment(Pos.CENTER);
-//	    titleLabel.setLayoutX(0);
-//	    titleLabel.setLayoutY(20);
-//	    titleLabel.setPrefWidth(scene.getWidth());
-//	    root.getChildren().add(titleLabel);
-//
-//	    // Convert the timerTask value to minutes and seconds format
-//	    long seconds = timerTask.scheduledExecutionTime() / 1000;
-//	    long minutes = seconds / 60;
-//	    long remainingSeconds = seconds % 60;
-//	    String timerString = String.format("%d:%02d", minutes, remainingSeconds);
-//
-//	    // Create a label for the timerTask value
-//	    Label timerLabel = new Label("Time taken: " + timerString);
-//	    timerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 18));
-//	    timerLabel.setAlignment(Pos.CENTER);
-//	    timerLabel.setLayoutX(0);
-//	    timerLabel.setLayoutY(70);
-//	    timerLabel.setPrefWidth(scene.getWidth());
-//	    root.getChildren().add(timerLabel);
-//
-//	    // Create a button for exiting the game
-//	    Button exitButton = new Button("Game Menu");
-//	    exitButton.setPrefWidth(120);
-//	    exitButton.setLayoutX(scene.getWidth() / 2 - exitButton.getPrefWidth() / 2);
-//	    exitButton.setLayoutY(100);
-//	    exitButton.setOnAction(e -> {
-//	        // Create a new instance of the GameMenu class
-//	        GameMenu gameMenu = new GameMenu();
-//	        try {
-//	            gameMenu.start(owner);
-//	        } catch (Exception e1) {
-//	            e1.printStackTrace();
-//	        }
-//	    });
-//	    root.getChildren().add(exitButton);
-//
-//	    // Show the window
-//	    owner.show();
-//	}
 
 
 
